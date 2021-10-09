d01b0a924193beb1d7d6d23e9c4dddff2f95385f
Jota Ernst <ju.swc25@gmail.com>
2021-10-09 00:39:19 -0300

    Added assets and styled section 2 slider for small screen sizes

--- scripts.js
+++ scripts.js
@@ -34,3 +34,37 @@ function changeHeader() {
        header.className = '';
     }
 }
+
+// ADAPT SLIDER ON SECTION 2 FOR SMALL SCREENS 
+
+let windowRes = window.matchMedia("(max-width: 830px)");
+
+let glideArrows = document.querySelector('.glide__arrows');
+
+function hideGlideArrows(windowRes) {
+    if (windowRes.matches) {
+        glideArrows.style.visibility = 'hidden';
+    }
+    else {
+        glideArrows.style.visibility = 'visible';
+    }
+}
+
+let glideSlide = document.querySelectorAll('.glide__slide');
+let glideSlideImg = [
+    '<img src="assets/slide-img1-mobile.svg" alt="Será medida a temperatura ao chegar no escritório.">',
+    '<img src="assets/slide-img2-mobile.svg" alt="Todos os consultores deverão usar máscaras dentro das acomodações dos escritórios.">',
+    '<img src="assets/slide-img3-mobile.svg" alt="Nas mesas existem adesivos sinalizando onde os #SangueLaranjas devem sentar, para que haja o distanciamento necessário.">',
+    '<img src="assets/slide-img4-mobile.svg" alt="Pessoas que estiverem sentindo algum sintoma não devem ir trabalhar presencialmente, a indicação é para que permaneçam em casa.">'
+];
+
+function switchImgs(windowRes) {
+    if (windowRes.matches) {
+        for (i = 0; i < glideSlide.length; i++) {
+            glideSlide[i].innerHTML = glideSlideImg[i];
+        }
+    }
+}
+
+hideGlideArrows(windowRes);
+switchImgs(windowRes);
